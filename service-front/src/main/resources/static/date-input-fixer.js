1a676e8482299b01a3eb8817186173487ce57b8a
dev <japanito@protonmail.com>
2025-06-21 17:05:52 +0200

    revert dao

--- service-front/src/main/resources/static/date-input-fixer.js
+++ service-front/src/main/resources/static/date-input-fixer.js
@@ -1,10 +1,28 @@
 (() => {
+    const parseDate = (dateStr, format = "DD/MM/YYYY") => {
+      const formatParts = format.split(/[^A-Z]/);
+      const dateParts = dateStr.split(/[^0-9]/);
+
+      const dateMap = {};
+      formatParts.forEach((part, i) => {
+        dateMap[part] = parseInt(dateParts[i], 10);
+      });
+
+      const day = dateMap["DD"] || 1;
+      const month = (dateMap["MM"] || 1) - 1;
+      const year = dateMap["YYYY"] || 1970;
+
+      return new Date(year, month, day);
+    };
+
     document.querySelectorAll('input[type="date"]').forEach(input => {
         const rawAttrValue = input.getAttribute('value');
         if(!rawAttrValue || rawAttrValue.trim().length == 0) return;
 
         try {
-            const parsedDate = new Date(rawAttrValue);
+            let parsedDate = new Date(rawAttrValue);
+            if(!parsedDate || isNaN(parsedDate)) parsedDate = parseDate(parsedDate);
+
             if(!parsedDate || isNaN(parsedDate)) throw new Error("Invalid date format");
 
             const year = parsedDate.getFullYear().toString().padStart(4, '0');
